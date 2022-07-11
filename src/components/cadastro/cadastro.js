ca66c2100ec63b33f46814ad8bed4928c6080211
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2022-07-11 10:54:13 -0300

    main

---
+++ src/components/cadastro/cadastro.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components'; 
+
+export const Container = styled.div`
+    background-color: white;
+    height: 90vh;
+    width: 90%;
+    border: 1px solid black;
+`;
+export const Header = styled.div`
+    border: 1.5px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    height: 5%;
+    width: 100%;
+    background-color: #f0f0f0;
+    button{
+        margin: 0 5px;
+        position: relative;
+    }
+`;
+export const Info = styled.div`
+    height: 40%;
+    display: block;
+    flex-wrap: wrap;
+    align-content: center;
+    padding-left: 10px;
+   
+    form{
+        display: flex;
+        flex-wrap: wrap;
+        align-content: center;
+        margin: 5px 0;
+        
+    }
+    .checkbox{
+        margin: 0 15px;
+        background-color: #339afe;
+        align-content: center;
+    }
+    .radio{
+        margin: 0 10px;
+    }
+    .cod{
+        margin-right: 30%;
+        margin-left: 15px;
+        width: 60px;
+    }
+    #line{
+        margin-left: 20px;
+        height: 20px;
+        width: 0;
+        border-right: 1px solid #000;
+    }
+    .f1{
+        width: 60px;
+    }
+    .option{
+        background-color: #f0f0f0;
+        width: 75vw;
+        margin: 0 10px;
+    }
+    .information{
+        margin: 5px 0;
+        align-content: center;
+        justify-content: end;
+    }
+    .partner{
+        background-color: #f0f0f0;
+        width: 55vw;
+        margin: 0 10px;
+    }
+    input{
+        margin-right: 10px;
+    }
+    label{
+        margin: 0 5px;
+    }
+    fieldset{
+       margin: 0 10px ;
+    }
+    
+`;
+export const Add = styled.div`
+    height: 15%;
+    align-items: center;
+`
+export const Display = styled.div`
+    background-color: #f0f0f0;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    height: 20vh;
+    width: 80%;
+`
---
+++ src/components/cadastro/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import * as C from './cadastro.js';
+
+export const Cadastro = () => {
+    return(
+        <C.Container>
+            <C.Header>
+                <button>Consultar</button><button>Cadastrar</button>
+            </C.Header>
+            <C.Info>
+                <form>
+                <label>Código da rotina: </label>
+                <input className="cod"></input>
+                <div id="line"></div>
+                <input type="radio" className="radio" name="Atacado"></input>
+                <label>Atacado</label>
+                <input type="radio" className="radio" name="Varejo"></input>
+                <label>Varejo</label>
+                <div id="line"></div>
+                <div className="checkbox">
+                    <input type="checkbox"/>
+                    <label>Aprovado</label>
+                    <input type="checkbox"/>
+                    <label>Cancelado</label>
+                    <input type="checkbox"/>
+                    <label>Entregue</label>
+                </div>
+                </form>
+                <form className="information">
+                    <label>Emitente: </label>
+                    <input className="f1"/>
+                    <input className="option"/>
+                    <label>T.O.P: </label>
+                    <input className="f1"/>
+                    <input className="option"/>
+                    <label>Vendedor: </label>
+                    <input className="f1"/>
+                    <input className="option"/>
+                    <label>Parceiro: </label>
+                    <input className="f1"/>
+                    <input className="partner"/>
+                    <label>CPF/CNPJ: </label>
+                    <input/>
+                    <label>Tipo pgto: </label>
+                    <input className="f1"/>
+                    <input className="option"/>
+                </form>
+                <fieldset><legend>Observação</legend>Observação</fieldset>
+            </C.Info>
+            <C.Header>
+                <button>Produtos</button><button>Financeiro</button>
+            </C.Header>
+            <C.Add>
+            <form>
+                <label>Código: </label>
+                <input/>
+                <label>Quantidade: </label>
+                <input/>
+                <label>Valor Unitário: </label>
+                <input list="#"></input>
+                <datalist></datalist>
+                <label>Desconto: </label>
+                <input/><input/>
+                <label>Tota do item: </label>
+                <input/>
+                <label>Descrição: </label>
+                <input/>
+            </form>
+            </C.Add>
+            <C.Display>
+
+            </C.Display>
+        </C.Container>   
+    );
+}
