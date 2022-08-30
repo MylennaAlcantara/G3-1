f8ce148880ba1bd80afe35d59295f54dfce7961f
Mylenna Alcantara <mylenna.a.a.corinthiana@gmail.com>
2022-08-30 18:01:11 -0300

    Implementação do slide tela de login

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,18 +1,61 @@
 import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import * as C from "./login.js";
 
 export const Login = () => {
+            
+    const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+    const [index, setIndex] = useState(0);
+    const delay = 4500;
+    const timeoutRef = useRef(null);
+
+    useEffect(() => {
+        setTimeout(
+            timeoutRef = setTimeout(
+            () => setIndex((prevIndex) =>
+                prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+            )),
+            delay
+        );
+        return () => {};
+    }, [index]);
+
     return(
+
         <C.Container>
-            <C.Image></C.Image>
+            <C.Image>
+                <div className="slideShow">
+                    <div className="slideshowSlider" style={{transform: `translate3d(${-index * 100}%, 0, 0)`}}>
+                       {colors.map((background, index) => (
+                        <div className="slide" key={index} style={{background}}></div>
+                       ))}  
+                    </div>
+                    <div className="slideshowDots">
+                        {colors.map(( _, idx) => (
+                            <div 
+                            key={idx}
+                            className={`slideshowDot${index === idx ? " active" : ""}`}
+                            onClick={() => {
+                                setIndex(idx);
+                            }} 
+                            > </div>
+                        ))}
+                    </div>
+                </div>
+            </C.Image>
             <C.Acessar>
-                <h1>Logo</h1>
-                <form>
-                    <label>Matricula: </label>
-                    <input/>
-                    <label>senha: </label>
-                    <input/>
-                </form>
+                
+                    <h1>Logo</h1>
+                    <form>
+                        <label>Matricula: </label>
+                        <input/><br/>
+                        <label>senha: </label>
+                        <input/><br/>  
+                        <button type="submit">Entrar</button>
+                    </form>
+              
             </C.Acessar>
         </C.Container>
     )
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,15 +4,55 @@ export const Container = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
+    justify-content: center;
+    align-items: center;
 `
 export const Image = styled.div`
     height: 100vh;
     width: 50vw;
+    justify-content: center;
+    align-items: center;
+    .slideShow{
+        margin: 0 auto;
+        overflow: hidden;
+        max-width: 500px;
+    }
+    .slideshowSlider{
+        white-space: nowrap;
+        transition: ease 1000ms;
+    }
+    .slide{
+        height: 400px;
+        width: 100%;
+        display: inline-block;
+        border-radius: 40px;
+    }
+    .slideshowDots{
+        text-align: center;
+    }
+    .slideshowDot{
+        display: inline-block;
+        height: 20px;
+        width: 20px;
+        border-radius: 50%;
+        cursor: pointer;
+        margin: 15px 7px 0px;
+        background-color: #c4c4c4;
+    }
+    .slideshowDot.active{
+        background-color: #0088FE;
+    }
 `
 export const Acessar = styled.div`
     height: 100vh;
     width: 50vw;
     background-color: white;
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    div{
+        display: flex;
+        justify-content: end;
+    }
 `
