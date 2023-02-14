3d496e86be12f4ffe5097352cabeaf5316851df9
Mylenna Alcantara Araujo <mylenna.a.a.corinthiana@gmail.com>
2023-02-14 14:32:00 -0300

    Filtro tela de tipo pagamento

--- src/components/modal_pgt/index.js
+++ src/components/modal_pgt/index.js
@@ -4,26 +4,32 @@ import {Container, Filtro, Header, Modal} from './../modal/modal.js';
 
 export const Pgt = ({onClose = () =>{}, setDataSelectPgt, setDataIdSelectPgt}) => {
 
-    const [users, setUsers] = useState([]);
+    const [pgto, setPgto] = useState([]);
     const [selectPgt, setSelectPgt] = useState();
     const [selectIdPgt, setSelectIdPgt] = useState();
+    const [busca, setBusca] = useState('');
 
     useEffect(() => {
         async function fetchData (){
             const response = await fetch("http://10.0.1.10:8092/tipoPagamento/all");
             const data = await response.json();
-            setUsers(data);
+         setPgto(data);
         }
             fetchData();
     }, []);
 
-    const SelectedPgt = (user) => {
-        setSelectPgt(user.descricao);
-        setSelectIdPgt(user.id);
-        setDataSelectPgt(user.descricao);
-        setDataIdSelectPgt(user.id);
+    const SelectedPgt = (pgto) => {
+        setSelectPgt(pgto.descricao);
+        setSelectIdPgt(pgto.id);
+        setDataSelectPgt(pgto.descricao);
+        setDataIdSelectPgt(pgto.id);
         onClose();
     };
+
+    //Filtro de busca
+    const resultado = Array.isArray(pgto) && pgto.filter((pgto) => {
+        return pgto.descricao.toLowerCase().includes(busca);
+    })
 
     return(
         <Modal>
@@ -35,7 +41,7 @@ export const Pgt = ({onClose = () =>{}, setDataSelectPgt, setDataIdSelectPgt}) =
             <Filtro>                        
                 <div className="div-search">
                     <label>Buscar: </label>                    
-                    <input className="search" placeholder="Buscar"/>
+                    <input className="search" placeholder="Buscar" onChange={e => setBusca(e.target.value)}/>
                 </div>                
             </Filtro>
                 <table id="table" >
@@ -48,12 +54,12 @@ export const Pgt = ({onClose = () =>{}, setDataSelectPgt, setDataIdSelectPgt}) =
                         </tr>
                     </thead>
                     <tbody>
-                        {users.slice(0, 10).map( (user) => {
+                        {resultado.map( (pgto) => {
                             return(
-                                <tr key={user.id} onDoubleClick={SelectedPgt.bind(this, user)} >
-                                    <td>{user.id}</td>
+                                <tr key={pgto.id} onDoubleClick={SelectedPgt.bind(this, pgto)} >
+                                    <td>{pgto.id}</td>
                                     <td>Sim</td>
-                                    <td>{user.descricao}</td>
+                                    <td>{pgto.descricao}</td>
                                     <td></td>
                                 </tr>
                             );
