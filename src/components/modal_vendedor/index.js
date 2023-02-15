fc1b3b90ff063472f8a8ad8558946a285e73ddb6
Mylenna Alcantara Araujo <mylenna.a.a.corinthiana@gmail.com>
2023-02-15 11:46:49 -0300

    Selecionar através do teclado na tela de vendedor

--- src/components/modal_vendedor/index.js
+++ src/components/modal_vendedor/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import {Container, Filtro, Header, Modal} from './../modal/modal.js';
 
 
@@ -27,8 +27,36 @@ export const Saler = ({onClose = () =>{}, setDataSelectSaler, setDataIdSelectSal
     };
 
     // Filtro de Busca
-    const resultado = Array.isArray(users) && users.filter((user) => user.nome.toLowerCase().includes(busca))
-    console.log(busca)
+    const resultado = Array.isArray(users) && users.filter((user) => user.nome.toLowerCase().includes(busca));
+
+    //selecionar o produto atraves da seta para baixo e para cima, adicionar o item pela tecla enter
+    const [selectIndex, setSelectIndex] = useState(0);
+    const tableRef = useRef(null);
+
+    const handleKeyDown = (e) => {
+        if(e.keyCode === 38){
+            e.preventDefault();
+            if(selectIndex === null || selectIndex === 0){
+                return;
+            }
+            setSelectIndex(selectIndex-1);
+        }else if (e.keyCode === 40){
+            e.preventDefault();
+            if(selectIndex === null || selectIndex === resultado.length -1 ){
+                return;
+            }
+            setSelectIndex(selectIndex + 1);
+        }else if (e.keyCode === 13){
+            e.preventDefault();
+            if(selectIndex !== null){
+                setSelectSaler(resultado[selectIndex].nome);
+                setSelectIdSaler(resultado[selectIndex].id);
+                setDataSelectSaler(resultado[selectIndex].nome);
+                setDataIdSelectSaler(resultado[selectIndex].id);
+                onClose();
+            }
+        }
+    };
 
     return(
         <Modal>
@@ -44,10 +72,10 @@ export const Saler = ({onClose = () =>{}, setDataSelectSaler, setDataIdSelectSal
                         </div>
                     </div>
                     <div className="div-search">
-                        <input className="search" placeholder="Buscar" onChange={e => setBusca(e.target.value)}/>
+                        <input className="search" placeholder="Buscar" onChange={e => setBusca(e.target.value)} onKeyDown={handleKeyDown}/>
                     </div>                
             </Filtro>
-                <table id="table" >
+                <table id="table" ref={tableRef} onKeyDown={handleKeyDown} tableRef={0} >
                     <thead>
                         <tr>
                             <th>Código</th>
@@ -56,12 +84,15 @@ export const Saler = ({onClose = () =>{}, setDataSelectSaler, setDataIdSelectSal
                         </tr>
                     </thead>
                     <tbody>
-                        {resultado.slice(0,10).map( (user) => {
+                        {resultado.slice(0,10).map( (user, index) => {
                             return(
-                                <tr key={user.id} onDoubleClick={SelectedSaler.bind(this, user)} >
-                                    <td>{user.id}</td>
-                                    <td>{user.nome}</td>
-                                    <td>{user.nome}</td>
+                                <tr 
+                                    key={user.id} 
+                                    onDoubleClick={SelectedSaler.bind(this, user)} 
+                                    style={{backgroundColor: index === selectIndex ? '#87CEFA' : ''}}>
+                                        <td>{user.id}</td>
+                                        <td>{user.nome}</td>
+                                        <td>{user.nome}</td>
                                 </tr>
                             );
                         })}
