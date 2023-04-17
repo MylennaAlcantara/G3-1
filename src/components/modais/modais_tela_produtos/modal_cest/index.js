41ea6b801172568818a456cad842e88f016a8b19
Mylenna Alcantara Araujo <mylenna.a.a.corinthiana@gmail.com>
2023-04-17 14:28:42 -0300

    Listagem e Filtragem de CEST

--- src/components/modais/modais_tela_produtos/modal_cest/index.js
+++ src/components/modais/modais_tela_produtos/modal_cest/index.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import * as M from "../../modal/modal";
+import cest from "../../../../cest/cest.json";
 
 export const Cest = ({close}) => {
+    const [filtro, setFiltro] = useState('cest');
+    const [busca, setBusca] = useState('');
+
+    function changeFiltro (e){
+        setFiltro(e.target.value);
+    }
+
+    const resultado = Array.isArray(cest) && cest.filter((cest)=> {
+        if(filtro === "cest"){
+            return String(cest.CEST).toLowerCase().includes(busca);
+        }else if(filtro === "desc"){
+            return cest.DESCRICAO.toLowerCase().includes(busca);
+        }else if(filtro === "ncm"){
+            return String(cest.NCM).toLowerCase().includes(busca);
+        }
+    })
+
     return(
         <M.Modal>
             <M.Container>
@@ -11,15 +29,15 @@ export const Cest = ({close}) => {
                 </M.Header>
                 <M.Filtro>
                     <div>
-                        <input type="radio" name="filtro"/>
+                        <input type="radio" value="cest" name="filtro" checked={filtro === 'cest'} onChange={changeFiltro}/>
                         <label>CEST</label>
-                        <input type="radio" name="filtro"/>
+                        <input type="radio" value="desc" name="filtro" checked={filtro === 'desc'} onChange={changeFiltro}/>
                         <label>Desc.</label>
-                        <input type="radio" name="filtro"/>
+                        <input type="radio" value="ncm" name="filtro" checked={filtro === 'ncm'} onChange={changeFiltro}/>
                         <label>NCM</label>
                     </div>
                     <div className="div-search">
-                        <input className="search" placeholder="Buscar..."/>
+                        <input className="search" placeholder="Buscar..." value={busca} onChange={(e)=> setBusca(e.target.value)}/>
                     </div>
                 </M.Filtro>
                 <div className="table-responsive">
@@ -32,7 +50,15 @@ export const Cest = ({close}) => {
                             </tr>
                         </thead>
                         <tbody>
-                            
+                            {resultado.map((cest)=> {
+                                return (
+                                    <tr key={cest.ID}>
+                                        <td>{cest.CEST}</td>
+                                        <td>{cest.NCM}</td>
+                                        <td>{cest.DESCRICAO}</td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
                 </div>
