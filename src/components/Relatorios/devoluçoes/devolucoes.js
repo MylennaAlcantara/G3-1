8eedff3f63e82da9734c85d6f3c3d16ccfb682e3
ViniciusMariano02 <viniciusvideo2300@gmail.com>
2023-08-14 16:58:45 -0300

    14/08

--- src/components/Relatorios/devoluçoes/devolucoes.js
+++ src/components/Relatorios/devoluçoes/devolucoes.js
@@ -1,1 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import * as M from "../../modais/modal/modal";
+import * as C from "../../cadastro/cadastro";
+import * as CV from "../comissao_por_vendedor/style"
+
+export const Devolucao = () => {
+
+    const [aba, setAba] = useState('Produto')
+
+    return (
+        <M.Modal>
+            <C.Container>
+                <C.Header>Relatórios de Devoluções</C.Header>
+
+                <CV.Filtros>
+                    <fieldset>
+                        <div>
+                            <input type="radio" />
+                            <label>Produto</label>
+
+                            <input type="radio" />
+                            <label>Documento</label>
+                        </div>
+                    </fieldset>
+
+                    {aba === 'Produto' ? (
+                        <>
+                            <input /> <img src="/images/LUPA.png" />
+                        </>
+                    ) : null}
+                </CV.Filtros>
+            </C.Container>
+        </M.Modal>
+    )
+
+}
+
+export default Devolucao;
